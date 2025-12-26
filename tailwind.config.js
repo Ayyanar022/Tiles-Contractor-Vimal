9749a5d6ad9e526986c163c38bb438c774b16e3d
Ayyanar <ayan.prof022@example.com>
2025-12-26 21:46:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1f2937",
+        secondary: "#b45309",
+        accent: "#f59e0b",
+        background: "#f9fafb",
+        muted: "#6b7280",
+      },
+    },
+  },
+  plugins: [],
+};
